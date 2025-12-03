dc7848181b479e3efcee44e5bfaacca069792542
后果 <430390470@qq.com>
2025-12-03 08:39:35 +0800

    基础框架构建

---
+++ src/main/java/com/example/bankmanagesystem/BankManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankmanagesystem/BankManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankmanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
